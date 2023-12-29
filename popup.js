6036665f2b07a059cbabc292acb0e369bf179b81
Ian <ihmorris@ualberta.ca>
2023-12-29 16:20:10 -0700

    popup.js now syncs/saves user's blacklisted websites to their browser account

--- popup.js
+++ popup.js
@@ -1,16 +1,31 @@
 // After popup loads, add event listeners for form submissions
 document.addEventListener("DOMContentLoaded", function(){
+    
     function addItemToList() {
         var new_item = document.getElementById("newItem").value;
-        // test
-        console.log(new_item);
+        let blacklistURLS;
         document.getElementById("newItem").value = ""; // Clears form element
+        
+        // Retrieves the user-defined URLs from storage
+        chrome.storage.sync.get("blacklistURLS", function(data) {
+            blacklistURLS = data.blacklistURLS || [];
+            console.log("Retrieved blacklistURLS:", blacklistURLS);
+            blacklistURLS.push(new_item);
+
+            // Prints each URL to terminal for easy debugging
+            blacklistURLS.forEach(function(url) {console.log(url)});
+        
+            // Saves the user-defined URLs to storage
+            chrome.storage.sync.set({ blacklistURLS: blacklistURLS }, function() {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+            } else {
+                console.log("User-defined URLs saved successfully");
+            }
+            });
+        });
     }
 
-    /**
-     * Storage logic goes here
-     * browser.storage.sync.set()
-     */
     
     // Event listener for button click
     document.getElementById("button1").addEventListener("click", addItemToList);
@@ -20,5 +35,8 @@ document.addEventListener("DOMContentLoaded", function(){
         event.preventDefault(); // Prevents the default form submission behavior
         addItemToList();
     });
+
+
+
 });
 
