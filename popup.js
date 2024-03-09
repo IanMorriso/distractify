6d91643c4d12ceb9518525e19cd47a914d4918f1
Ian <ihmorris@ualberta.ca>
2024-03-09 12:15:25 -0700

    :)

--- popup.js
+++ popup.js
@@ -1,6 +1,16 @@
+import Semaphore from "./semaphore.js";
+
 // After popup loads, add event listeners for form submissions
 document.addEventListener("DOMContentLoaded", function(){
-    
+
+
+    // Retrieves the user-defined URLs from storage
+    //chrome.storage.sync.get("blacklistURLS", function(data) {
+    //    blacklistURLS = data.blacklistURLS || [];
+    //    console.log("Retrieved blacklistURLS:", blacklistURLS);
+    //});
+
+
     function addItemToList() {
         var new_item = document.getElementById("newItem").value;
         let blacklistURLS;
@@ -39,11 +49,11 @@ document.addEventListener("DOMContentLoaded", function(){
     
     // temp list of effects
     const effects = [
-        { id: 1, name: "Obscure"},
-        { id: 2, name: "Word-Scramble"},
-        { id: 3, name: "Load-Blocker"},
-        { id: 4, name: "Scroller"},
-        { id: 5, name: "Bouncing Ball"},
+        { id: 1, name: "Obscure", active: false},
+        { id: 2, name: "Word Scramble", active: false},
+        { id: 3, name: "Load Blocker", active: false},
+        { id: 4, name: "Scroller", active: false},
+        { id: 5, name: "Bouncing Ball", active: false},
     ];
     const itemsContainer = document.getElementById("container-items");
 
@@ -64,7 +74,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // Master-Toggle control
     let debounceTimeout;    // Needed as event listener triggered twice on a single click
+    //const debouncedMasterToggleHandler = debounce(masterToggleHandler, 200);
     const masterToggle = document.getElementById("master-toggle");
+    /*masterToggle.addEventListener("click", function(event) {
+        event.stopPropagation();
+        debouncedMasterToggleHandler();
+    });*/
     masterToggle.addEventListener("click", function (event) { 
         console.log(masterToggle.checked);
         event.stopPropagation();
@@ -84,22 +99,70 @@ document.addEventListener("DOMContentLoaded", function(){
         }, 200);
     });
 
-    //const toggles = document.querySelectorAll("[class='container-effects']");
+    function masterToggleHandler() {
+        const toggles = document.querySelectorAll("[id^='item-']");
+        toggles.forEach(toggle => {
+            const changeEvent = new Event("change", { bubbles: true });
+            if (masterToggle.checked && !toggle.checked) {
+                toggle.checked = true;
+                toggle.dispatchEvent(changeEvent);
+            } else if (!masterToggle.checked && toggle.checked) {
+                toggle.checked = false;
+                toggle.dispatchEvent(changeEvent);
+            }
+        });
+    }
+
+    chrome.storage.sync.get(null, function(items) {
+        console.log(items);
+    });
+    const sem = new Semaphore(1);
     const toggles = document.querySelectorAll("[id^='item-']");
     toggles.forEach(function(toggle) {
-        toggle.addEventListener("change", function() {
+        toggle.addEventListener("change", async function() {
             if (toggle.checked) {
                 console.log(`${toggle.id} is checked`);
+
             } else {
                 // Checkbox is unchecked
                 console.log(`${toggle.id} is unchecked`);
             }
+
+            await chrome.storage.sync.get('tests', function(result) {
+                checkAndSetToggles('tests', result, toggle);
             });
+            /*
+            await chrome.storage.sync.get('tests', async function(result) {
+                if ('tests' in result && result.tests !== undefined) {
+                    var retrievedObjs = JSON.parse(result.tests);
+                    var objtoedit = retrievedObjs.find(o => o.name == toggle.id.split('-')[1]);
+                    objtoedit.active = toggle.checked;
+                    
+                    await chrome.storage.sync.set({'tests':JSON.stringify(retrievedObjs)}, function() {
+                        console.log("ya did it");
+                    });
+                } else {
+                    console.log(" 'tests' not found in chrome.storage.sync");
+                }
+              });*/
+
+        });
+
     });
 
-    chrome.runtime.sendMessage({ contentScriptLoaded: true });
-
-
-    
+    chrome.runtime.sendMessage({ contentScriptLoaded: true });   
 });
 
+async function checkAndSetToggles(tests, result, toggle) {
+    if ('tests' in result && result.tests !== undefined) {
+        var retrievedObjs = JSON.parse(result.tests);
+        var objtoedit = retrievedObjs.find(o => o.name == toggle.id.split('-')[1]);
+        objtoedit.active = toggle.checked;
+        await console.log(retrievedObjs);
+        await chrome.storage.sync.set({'tests':JSON.stringify(retrievedObjs)}, function() {
+            console.log("ya did it");
+        });
+    } else {
+        console.log(" 'tests' not found in chrome.storage.sync");
+    }
+}
